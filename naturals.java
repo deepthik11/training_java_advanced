047cdf3fa47963a613b47ecd743d4f64bc18a381
deepthik11 <deepthikhub@gmail.com>
2026-07-08 15:59:07 +0530

    Add files via upload

---
+++ naturals.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class naturals {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+    }
+    
+}
---
+++ reverse.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class reverse {
+    public static void main(String[] args) {
+        int num = 12345;
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed number: " + reversed);
+    }
+    
+}
